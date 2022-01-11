42acb10a901261b7beea820e4796655fec6b609e
BitJKit <ionathan.m.kelly@gmail.com>
2022-01-11 11:56:01 -0500

    MainActivity Toast init

--- app/src/main/java/com/example/noactivitiy_jan10/MainActivity.kt
+++ app/src/main/java/com/example/noactivitiy_jan10/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.noactivitiy_jan10
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 // Extending AppCompatActivity to let this class know its an activity
@@ -9,5 +10,6 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext, "This is a toast.", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/noactivitiy_jan10/SecondActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.noactivitiy_jan10
-
-import androidx.appcompat.app.AppCompatActivity
-
-class SecondActivity: AppCompatActivity() {
-}
